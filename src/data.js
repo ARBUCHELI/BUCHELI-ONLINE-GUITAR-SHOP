22419089bb698e3ae3d1cc34ee6f507ec960e40c
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-10-26 23:37:45 +0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const inventoryData = [
+    { name: 'Fender Telecaster Player', price: 850, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-player-telecaster-mn-3-c.jpg' },
+    { name: "Fender Stratocaster", price: 500, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-standard-stratocaster-mn-bla.jpg' },
+    { name: 'Fender Jaguar Master', price: 879, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-jaguar-master.jpg' },
+    { name: 'Fender Mustang', price: 1000, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-american-performer-mustang-r.jpg' },
+    { name: 'Fender Jazzmaster', price: 1500, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-60s-jazzmaster-sfg-lacquer-s.jpg' },
+    { name: 'Fender Duo Sonic', price: 799, img: 'https://raw.githubusercontent.com/ARBUCHELI/BUCHELI-ONLINE-GUITAR-SHOP/master/images/fender-duo-sonic-hs-pau-ferro-bk_1_.jpg' },
+  ];
+  
+  export const currenciesData = ['USD', 'EUR', 'RUB'];
